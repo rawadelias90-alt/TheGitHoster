31a70e1f5eebdbbce994a92f3a9cd2510a156b1e
rawadelias90-alt <rawad.elias90@gmail.com>
2026-09-17 20:41:32 +0400

    test: align production rules expectations to updated source

--- Intake2/tests/production-rules.test.js
+++ Intake2/tests/production-rules.test.js
@@ -3,7 +3,7 @@ const path = require('node:path');
 
 const rules = require(path.resolve(__dirname, '..', 'production-rules.js'));
 
-assert.equal(rules.version, '1.0');
+assert.equal(rules.version, '1.1');
 assert.equal(rules.requestId.prefix, 'REQ');
 assert.equal(rules.terminology.route, 'Onboarding Path');
 assert.equal(rules.fileRules.maxSizeMB, 10);
@@ -20,9 +20,9 @@ assert.equal(docs.passportCopy.requirement, 'required');
 assert.equal(docs.candidatePhoto.requirement, 'required');
 assert.equal(docs.signedOffer.requirement, 'required');
 assert.equal(docs.educationCertificate.requirement, 'conditional');
-assert.equal(docs.educationCertificate.when, 'skilledApplicable');
+assert.equal(docs.educationCertificate.when, 'applicableSkilledClassification');
 assert.equal(docs.policeClearance.requirement, 'conditional');
-assert.equal(docs.externalCoverPassport.when, 'dubaiOrDwc');
+assert.equal(docs.externalCoverPassport.when, 'dubaiOrDwcAndApplicable');
 
 assert.equal(rules.nationalDocuments.emirati.familyBook, 'required');
 assert.equal(rules.nationalDocuments.emirati.nationalId, 'required');
@@ -35,8 +35,8 @@ assert.deepEqual(rules.specialHire.freeZone.nationalities.sort(), ['Egypt', 'Sri
 assert.deepEqual(rules.specialHire.intakeDocuments, ['homeCountryNationalId']);
 assert.deepEqual(rules.specialHire.laterProcessOnly.sort(), ['homeCountryMedical', 'uaeEmbassyProcess'].sort());
 
-assert.equal(rules.education.ifEquivalencyAvailable, 'certificateOfEquivalency');
-assert.equal(rules.education.ifEquivalencyUnavailable, 'awardEducationDetailsDocument');
+assert.equal(rules.education.ifEquivalencyAvailable, 'certificateOfEquivalencyOrEducationalVerification');
+assert.equal(rules.education.ifEquivalencyUnavailable, 'awardOrEducationDetailsDocument');
 assert.equal(rules.additionalSupportingDocuments.requirement, 'optional');
 
 console.log('Intake2 production rules test passed');
